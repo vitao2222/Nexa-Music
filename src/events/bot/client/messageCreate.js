a258050c573e3a3f2c5d8fa8c184c47b4fe0cf79
KoDdy <akshaykb0101@gmail.com>
2025-09-05 13:43:40 +0000

    Update messageCreate.js

--- src/events/bot/client/messageCreate.js
+++ src/events/bot/client/messageCreate.js
@@ -55,7 +55,7 @@ client.on("messageCreate", async (message) => {
  * Project: Nexa Music
  * Author: KoDdy
  * Company: Infinity
- * This code is the property of EnourDev and may not be reproduced or
+ * This code is the property of Infinity and may not be reproduced or
  * modified without permission. For more information, contact us at
  * https://discord.gg/fbu64BmPFD
  */
