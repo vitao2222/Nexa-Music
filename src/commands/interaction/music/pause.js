4d86e69614252ac7706cd382fbecf293d605be1d
KoDdy <akshaykb0101@gmail.com>
2025-09-05 13:48:42 +0000

    Update pause.js

--- src/commands/interaction/music/pause.js
+++ src/commands/interaction/music/pause.js
@@ -21,7 +21,7 @@ module.exports = {
  * Project: Nexa Music
  * Author: KoDdy
  * Company: Infinity
- * This code is the property of EnourDev and may not be reproduced or
+ * This code is the property of Infinity and may not be reproduced or
  * modified without permission. For more information, contact us at
  * https://discord.gg/fbu64BmPFD
  */
