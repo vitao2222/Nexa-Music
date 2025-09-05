b5932e5b75c2c576a8d002a26bbc33d582572428
KoDdy <akshaykb0101@gmail.com>
2025-09-05 13:40:42 +0000

    Update convert.js

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -67,3 +67,12 @@ module.exports = {
     return s*1000;
   }
 						 }
+
+/**
+ * Project: Nexa Music
+ * Author: KoDdy
+ * Company: Infinity
+ * This code is the property of Infinity and may not be reproduced or
+ * modified without permission. For more information, contact us at
+ * https://discord.gg/fbu64BmPFD
+ */
